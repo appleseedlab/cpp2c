4da74206c4e251b1a6ecef2eb591449dc639acac
Brent Pappas <pappasbrent@gmail.com>
2021-08-19 08:09:50 -0400

    create unittest test script

---
+++ tests/test_collect_macro_data.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from macro_data_collector import collect_macro_data
+
+
+class CollectMacroDataTests(TestCase):
+    def test_test(self):
+        self.assertEqual(True, True)
