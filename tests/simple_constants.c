0e34408d4261dec3ab9a1696e378f1d9070acbdd
Brent Pappas <pappasbrent@gmail.com>
2021-08-19 08:32:53 -0400

    simple constant tests for macro data collection

---
+++ tests/simple_constants.c
@@ -0,0 +1,13 @@
+// Macro    |   Expected Type
+#define A 1  //  int
+#define B -1 //  int
+
+/*
+TODO: Add more qualifiers for numeric types:
+short
+long
+unsigned
+long long
+[negative values]
+*/
+// TODO: Add more types: char, string, float, double
--- tests/test_collect_macro_data.py
+++ tests/test_collect_macro_data.py
@@ -1,7 +1,24 @@
+import os
+from typing import Tuple
 from unittest import TestCase
-from macro_data_collector import collect_macro_data
+
+from macro_data_collector.collect_macro_data import collect_macro_data
+from macro_data_collector.directives import ObjectDefine
+
+
+def test_file_paths(fn: str) -> Tuple[str, str]:
+    cur_dir = os.path.dirname(__file__)
+    return (os.path.join(cur_dir, fn + ".txt"),
+            os.path.join(cur_dir, fn + ".c"))
 
 
 class CollectMacroDataTests(TestCase):
-    def test_test(self):
-        self.assertEqual(True, True)
+
+    def test_collect_simple_constants(self):
+        stats_file, c_file = test_file_paths("simple_constants")
+        result = collect_macro_data(stats_file, c_file)
+        self.assertEqual(
+            result,
+            [ObjectDefine(file='/home/bpappas/cpp_to_c/tests/simple_constants.c', line=2, column=1, definition_count=1, identifier='A', body='1  //  int'),
+             ObjectDefine(file='/home/bpappas/cpp_to_c/tests/simple_constants.c', line=3, column=1, definition_count=1, identifier='B', body='-1 //  int')]
+        )
