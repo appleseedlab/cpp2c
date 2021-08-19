4d20ab63c19dbfe4b411529f9cec7c0c25518591
Brent Pappas <pappasbrent@gmail.com>
2021-08-19 09:03:00 -0400

    move test files to subdirectory

---
+++ tests/c_files_and_stat_files/positive_int_macros.c
@@ -0,0 +1,3 @@
+#define A 1
+#define B 2
+#define C 3
--- tests/test_collect_macro_data.py
+++ tests/test_collect_macro_data.py
@@ -8,8 +8,9 @@ from macro_data_collector.directives import ObjectDefine
 
 def test_file_paths(fn: str) -> Tuple[str, str]:
     cur_dir = os.path.dirname(__file__)
-    return (os.path.join(cur_dir, fn + ".txt"),
-            os.path.join(cur_dir, fn + ".c"))
+    test_input_dir = "c_files_and_stat_files"
+    return (os.path.join(cur_dir, test_input_dir, fn + ".txt"),
+            os.path.join(cur_dir, test_input_dir, fn + ".c"))
 
 
 class CollectMacroDataTests(TestCase):
@@ -19,6 +20,6 @@ class CollectMacroDataTests(TestCase):
         result = collect_macro_data(stats_file, c_file)
         self.assertEqual(
             result,
-            [ObjectDefine(file='/home/bpappas/cpp_to_c/tests/simple_constants.c', line=2, column=1, definition_count=1, identifier='A', body='1  //  int'),
-             ObjectDefine(file='/home/bpappas/cpp_to_c/tests/simple_constants.c', line=3, column=1, definition_count=1, identifier='B', body='-1 //  int')]
+            [ObjectDefine(file='/home/bpappas/cpp_to_c/tests/c_files_and_stat_files/simple_constants.c', line=2, column=1, definition_count=1, identifier='A', body='1  //  int'),
+             ObjectDefine(file='/home/bpappas/cpp_to_c/tests/c_files_and_stat_files/simple_constants.c', line=3, column=1, definition_count=1, identifier='B', body='-1 //  int')]
         )
