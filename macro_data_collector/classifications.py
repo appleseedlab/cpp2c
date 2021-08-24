26ab47d3143c97ccb7f8fca9a46163ca708dbf5e
Brent Pappas <pappasbrent@gmail.com>
2021-08-24 10:30:12 -0400

    Use pycparser to parse bodies of simple constant object macros

--- macro_data_collector/classifications.py
+++ macro_data_collector/classifications.py
@@ -5,35 +5,8 @@ Classes for classifying macros
 '''
 
 from dataclasses import dataclass
-from enum import Enum
-from typing import NewType, Union
 
 from macro_data_collector import directives
-
-
-class CType(Enum):
-    '''
-    Enum for C data types.
-    Types take from:
-    https://www.tutorialspoint.com/cprogramming/c_data_types.htm
-    '''
-    CHAR = "char"
-    UNSIGNED_CHAR = "unsigned char"
-    SIGNED_CHAR = "signed char"
-    INT = "int"
-    UNSIGNED_INT = "unsigned int"
-    SHORT = "short"
-    UNSIGNED_SHORT = "unsigned short"
-    LONG = "long"
-    UNSIGNED_LONG = "unsigned long"
-    FLOAT = "float"
-    DOUBLE = "double"
-    LONG_DOUBLE = "long double"
-
-    STRING = "char *"
-
-
-CTypeValue = NewType("CTypeValue", str)
 
 
 @dataclass
@@ -43,35 +16,10 @@ class ClassifiedMacro():
 
 
 @dataclass
-class SimpleConstantObject(ClassifiedMacro):
+class SimpleConstantMacro(ClassifiedMacro):
     '''Class for simple constant object macros'''
-    # Note: This string should be a value from the CType enum
-    # The datatype is not CType enum due to the fact that dataclasses
-    # containing enums cannot be easily converted to dicts with the asdict
-    # function
-    ctype: CTypeValue
+    ctype: str
     value: str
-
-
-@dataclass
-class NumberMacro(SimpleConstantObject):
-    '''Class for simple constant object macros defined to a number value'''
-    value: Union[int, float]
-    base: int = 10
-
-
-@dataclass
-class CharMacro(SimpleConstantObject):
-    '''Class for simple constant object macros defined to a char value'''
-    value: str
-
-
-@dataclass
-class StringMacro(SimpleConstantObject):
-    value: str
-
-    def __init__(self, macro: directives.DefineDirective, value: str):
-        super(StringMacro, self).__init__(macro, CType.STRING.value, value)
 
 
 @dataclass
--- macro_data_collector/classify_macros.py
+++ macro_data_collector/classify_macros.py
@@ -5,71 +5,11 @@ Roughly classifies macros based on their types and definitions.
 '''
 
 from typing import Union
+
+from pycparser.c_ast import Constant, UnaryOp
+from pycparser.c_parser import CParser
 from macro_data_collector import directives
-from macro_data_collector.classifications import (CharMacro, ClassifiedMacro, CType, NumberMacro, StringMacro,
-                                                  UnclassifiableMacro)
-from macro_data_collector.constants import CLimits
-import re
-
-# TODO: Figure out how to use pycparser to parse C code using Python.
-#       Can possibly use this to infer types from macro bodies.
-
-# Notice the [^0]. This is to prevent recognizing octals as ints
-INT_PATTERN = re.compile(r"^[+-]?[^0]\d*$")
-FLOAT_PATTERN = re.compile(r"^[+-]?(((\d+)?\.(\d+))|((\d+)\.(\d+)?))$")
-BINARY_PATTERN = re.compile(r"^[+-]?0(b|B)[01]+$")
-OCTAL_PATTERN = re.compile(r"^[+-]?0[0-7]+$")
-HEX_PATTERN = re.compile(r"^[+-]?0(x|X)[0-9A-Fa-f]+$")
-
-
-def get_ctype_from_number(val: Union[int, float]) -> CType:
-    '''
-    Returns a number's corresponding C data type
-
-    Args:
-        val:        The value to get the C type for
-        is_int:     Whether the number is an int or a float
-
-    Returns:
-        ctype:      The C type for the given values
-
-    Raises:
-        ValueError: If a macro is defined to an invalid number
-    '''
-    if isinstance(val, int):
-        if 0 <= val <= CLimits.UCHAR_MAX:
-            return CType.UNSIGNED_CHAR
-        elif CLimits.SCHAR_MIN <= val <= CLimits.SCHAR_MAX:
-            return CType.SIGNED_CHAR
-        elif CLimits.SHRT_MIN <= val <= CLimits.SHRT_MAX:
-            return CType.SHORT
-        elif 0 <= val <= CLimits.USHRT_MAX:
-            return CType.UNSIGNED_SHORT
-        elif CLimits.INT_MIN <= val <= CLimits.INT_MAX:
-            return CType.INT
-        elif 0 <= val <= CLimits.UINT_MAX:
-            return CType.UNSIGNED_INT
-        elif CLimits.LONG_MIN <= val <= CLimits.LONG_MAX:
-            return CType.LONG
-        elif 0 <= val <= CLimits.ULONG_MAX:
-            return CType.UNSIGNED_LONG
-        elif -CLimits.FLT_MAX <= val <= CLimits.FLT_MAX:
-            return CType.FLOAT
-        elif -CLimits.DBL_MAX <= val <= CLimits.DBL_MAX:
-            return CType.DOUBLE
-        elif -CLimits.LDBL_MAX <= val <= CLimits.LDBL_MAX:
-            return CType.LONG_DOUBLE
-        else:
-            raise ValueError(f"Invalid C number: {val}")
-    # Floats
-    if -CLimits.FLT_MAX <= val <= CLimits.FLT_MAX:
-        return CType.FLOAT
-    elif -CLimits.DBL_MAX <= val <= CLimits.DBL_MAX:
-        return CType.DOUBLE
-    elif -CLimits.LDBL_MAX <= val <= CLimits.LDBL_MAX:
-        return CType.LONG_DOUBLE
-    else:
-        raise ValueError(f"Invalid C number: {val}")
+from macro_data_collector.classifications import ClassifiedMacro, SimpleConstantMacro, UnclassifiableMacro
 
 
 def classify_macro(macro: directives.CPPDirective) -> ClassifiedMacro:
@@ -80,51 +20,30 @@ def classify_macro(macro: directives.CPPDirective) -> ClassifiedMacro:
         macro:  The macro to classify
 
     Returns: The macro along with its classification
-
-    Raises:
-        ValueError: If a macro is defined to an invalid value
     '''
-    # TODO: Need a way of ignoring comments in macro bodies...
+
     if isinstance(macro, directives.ObjectDefine):
-        # Classifying an object-like macro
-        if re.match(INT_PATTERN, macro.body):
-            val = int(macro.body)
-            ctype = get_ctype_from_number(val)
-            return NumberMacro(macro, ctype.value, val)
-        elif re.match(FLOAT_PATTERN, macro.body):
-            val = float(macro.body)
-            ctype = get_ctype_from_number(val)
-            return NumberMacro(macro, ctype.value, val)
-        elif re.match(BINARY_PATTERN, macro.body):
-            val = int(macro.body, base=2)
-            ctype = get_ctype_from_number(val)
-            return NumberMacro(macro, ctype.value, base=2)
-        elif re.match(OCTAL_PATTERN, macro.body):
-            val = int(macro.body, base=8)
-            ctype = get_ctype_from_number(val)
-            return NumberMacro(macro, ctype.value, base=8)
-        elif re.match(HEX_PATTERN, macro.body):
-            val = int(macro.body, base=16)
-            ctype = get_ctype_from_number(val)
-            return NumberMacro(macro, ctype.value, base=16)
+        # First, try to parse the body as a literal.
+        # Since C requires that literals be placed inside functions, first
+        # wrap the literal in a function
+        try:
+            c_function = "void f() {" + macro.body + ";}"
+            block_item: Union[Constant, UnaryOp] = CParser().parse(
+                c_function).ext[0].body.block_items[0]
+            constant: Constant
+            value: str
+            if isinstance(block_item, UnaryOp):
+                constant = block_item.expr
+                value = block_item.op + constant.value
+            elif isinstance(block_item, Constant):
+                constant = block_item
+                value = constant.value
+            else:
+                raise ValueError(
+                    "Object define macro body is not a constant"
+                    " or unary operation:", macro)
+            return SimpleConstantMacro(macro, constant.type, value)
+        except:
+            return UnclassifiableMacro(macro)
 
-        # Single character char
-        # TODO: Make sure this recognizes control characters
-        # and the single quote character correctly
-        elif (len(macro.body) == 3
-              and macro.body[0] == "'"
-              and macro.body[1] in [chr(i) for i in range(32, 127)]
-              and macro.body[2] == "'"):
-            return CharMacro(macro, CType.CHAR.value, macro.body[1])
-
-        # TODO: Determine if a macro body is actually a valid string.
-        # Currently just checking if the body begins and ends
-        # with double quotes...
-        elif (macro.body[0] == '"'
-              and macro.body[-1] == '"'):
-            return StringMacro(macro, macro.body)
-
-    elif isinstance(macro, directives.FunctionDefine):
-        # Classifying a function-like macro
-        pass
     return UnclassifiableMacro(macro)
