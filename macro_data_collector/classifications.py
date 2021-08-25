8336053f240ed3fe15ae32ad6ddabc9967c01026
Brent Pappas <pappasbrent@gmail.com>
2021-08-25 18:30:57 -0400

    Add emit method to emit converted macro

--- macro_data_collector/classifications.py
+++ macro_data_collector/classifications.py
@@ -20,6 +20,23 @@ class SimpleConstantMacro(ClassifiedMacro):
     '''Class for simple constant object macros'''
     ctype: str
     value: str
+    # TODO: Read AST to determine if an object macro is used as a case label
+    used_as_case_label: bool = False
+    # TODO: Group object macros used as case labels in the same switch
+    # statement in the same enum
+    enum_group_name: str = ""
+
+    def emit(self) -> str:
+        '''Emit the C declaration and initialization for the macro'''
+        ctype = self.ctype
+        if ctype == "string":
+            ctype = "char *"
+        if self.used_as_case_label:
+            # TODO: Emit all object defines used as case labels in the
+            # same switch statement to values in the same enum
+            pass
+
+        return f"const {ctype} {self.macro.identifier} = {self.value};"
 
 
 @dataclass
