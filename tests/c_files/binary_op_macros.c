2767c7d2627db722c93ba49d885630c22138b076
Brent Pappas <pappasbrent@gmail.com>
2021-09-13 08:20:11 -0400

    Add test cases for binary operations

---
+++ tests/c_files/binary_op_macros.c
@@ -0,0 +1,45 @@
+#define IDENT1 1.0L + 1.0L  // long double
+#define IDENT2 1.0L + 1.0   // long double
+#define IDENT3 1.0L + 1ULL  // long double
+#define IDENT4 1.0L + 1LL   // long double
+#define IDENT5 1.0L + 1UL   // long double
+#define IDENT6 1.0L + 1L    // long double
+#define IDENT7 1.0L + 1U    // long double
+#define IDENT8 1.0L + 1     // long double
+#define IDENT9 1.0L + 'a'   // long double
+#define IDENT10 1.0 + 1.0   // double
+#define IDENT11 1.0 + 1ULL  // double
+#define IDENT12 1.0 + 1LL   // double
+#define IDENT13 1.0 + 1UL   // double
+#define IDENT14 1.0 + 1L    // double
+#define IDENT15 1.0 + 1U    // double
+#define IDENT16 1.0 + 1     // double
+#define IDENT17 1.0 + 'a'   // double
+#define IDENT18 1ULL + 1ULL // unsigned long long int
+#define IDENT19 1ULL + 1LL  // unsigned long long int
+#define IDENT20 1ULL + 1UL  // unsigned long long int
+#define IDENT21 1ULL + 1L   // unsigned long long int
+#define IDENT22 1ULL + 1U   // unsigned long long int
+#define IDENT23 1ULL + 1    // unsigned long long int
+#define IDENT24 1ULL + 'a'  // unsigned long long int
+#define IDENT25 1LL + 1LL   // long long int
+#define IDENT26 1LL + 1UL   // long long int
+#define IDENT27 1LL + 1L    // long long int
+#define IDENT28 1LL + 1U    // long long int
+#define IDENT29 1LL + 1     // long long int
+#define IDENT30 1LL + 'a'   // long long int
+#define IDENT31 1UL + 1UL   // unsigned long int
+#define IDENT32 1UL + 1L    // unsigned long int
+#define IDENT33 1UL + 1U    // unsigned long int
+#define IDENT34 1UL + 1     // unsigned long int
+#define IDENT35 1UL + 'a'   // unsigned long int
+#define IDENT36 1L + 1L     // long int
+#define IDENT37 1L + 1U     // long int
+#define IDENT38 1L + 1      // long int
+#define IDENT39 1L + 'a'    // long int
+#define IDENT40 1U + 1U     // unsigned int
+#define IDENT41 1U + 1      // unsigned int
+#define IDENT42 1U + 'a'    // unsigned int
+#define IDENT43 1 + 1       // int
+#define IDENT44 1 + 'a'     // int
+#define IDENT45 'a' + 'a'   // char
