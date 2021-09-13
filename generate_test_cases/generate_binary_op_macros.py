a33edb6f21e9ae9ae714c30a5270a0b9c524bc4f
Brent Pappas <pappasbrent@gmail.com>
2021-09-13 09:10:45 -0400

    Generate test cases for macro classification

--- generate_test_cases/generate_binary_op_macros.py
+++ generate_test_cases/generate_binary_op_macros.py
@@ -1,9 +1,10 @@
 '''
 generate_test_cases.py
 
-Generate test cases for converting macros involving binary expressions
-'''
+Generate test cases for converting macros involving binary expressions.
 
+Possible code smell: Tight coupling here with testing setup.
+'''
 
 hierarchy_examples = [
     ('long double', '1.0L'),
@@ -18,20 +19,37 @@ hierarchy_examples = [
 ]
 
 
-results = []
+macro_definitions = []
+macro_data_collection_results = []
+macro_data_classification_results = []
 
 line_no = 1
 for i, (h1, left) in enumerate(hierarchy_examples):
     for h2, right in hierarchy_examples[i:]:
-        expected_type = ""
         expected_type = h1
         body = f"{left} + {right}"
         name = f"IDENT{line_no}"
-        print(f"#define {name} {body} // {expected_type}")
-        results.append(
-            f"ObjectDefine(c_file, {line_no}, {line_no}, 1, 1, '{name}', \"{body}\"),"
-        )
+        definition = f"#define {name} {body} // {expected_type}"
+        macro_definitions.append(definition)
+
+        macro_data_collection_result = f"ObjectDefine(c_file, {line_no}, {line_no}, 1, 1, '{name}', \"{body}\")"
+        macro_data_collection_results.append(macro_data_collection_result)
+        macro_data_classification_result = f"SimpleExpressionMacro({macro_data_collection_result}, '{expected_type}', \"{body}\")"
+        macro_data_classification_results.append(
+            macro_data_classification_result)
         line_no += 1
 
+print(*macro_definitions, sep="\n")
+
+# Print using list comprehensions for proper indentation
 print()
-print(*results, sep='\n')
+print("Expected result for macro data collection test cases:")
+print("[")
+[print("    " + result + ",") for result in macro_data_collection_results]
+print("]")
+
+print()
+print("Expected result for macro classification test cases:")
+print("[")
+[print("    " + result + ",") for result in macro_data_classification_results]
+print("]")
