32b9fab0086d00b35c76be199dc8fcde7e19bb67
Brent Pappas <pappasbrent@gmail.com>
2022-01-04 10:18:13 -0500

    Edit to work with changed argument order for invocation and definition; edit to work with added config var G; added theorem about expression evaluation being sound under function definition list transformation

--- proof_of_soundness/Theorems.v
+++ proof_of_soundness/Theorems.v
@@ -10,45 +10,81 @@ From Cpp2C Require Import EvalRules.
 From Cpp2C Require Import Transformations.
 
 
-(* A call to a macro without side-effects is equivalent to a call
-   to the transformed version of that macro as a function *)
-Lemma simple_macro_eq_func_call :
-  forall (S S': state) (E : environment)
-         (F F': func_definitions) (M M': macro_definitions)
-         (mexpr: expr) (x fname : string) (es : list expr)
-         (v : Z),
-  definition x F = None ->
-  invocation x M = Some mexpr ->
-  definition x F' = Some (Skip, mexpr) ->
-  invocation x M' = None ->
-  exprevalR S E F M mexpr v S' ->
-  exprevalR S E F' M' mexpr v S' ->
-  exprevalR S E F M (CallOrInvocation x es) v S' <-> exprevalR S E F' M' (CallOrInvocation x es) v S'.
-Proof.
-  intros.
-  split.
-  - intros.
-    + eapply E_FunctionCall.
-      * apply H1.
-      * apply E_Skip.
-      * apply H4.
-  - intros. eapply E_MacroInvocation.
-    * apply H0.
-    * apply H3.
-Qed.
-
-
 (* We currently need this theorem for the soundndess proof.
    It basically says that if an expression could be evaluated under the
    original function list, it can be evaluated correctly under the
    transformed function list as well. We know this will be true in
    the implementation since we will guarantee that all the function
    names will be unique, but we need this for the proof. *)
-Lemma eval_same_under_unique_names :
-  forall S E F M v S' x mexpr,
-  exprevalR S E F M mexpr v S' ->
-  exprevalR S E (((x ++ "__as_function")%string, (Skip, mexpr)) :: F) M mexpr v S'.
+Lemma expr_eval_same_under_unique_names :
+  forall S E G F M v S' x mexpr,
+  exprevalR S E G F M mexpr v S' ->
+  exprevalR S E G (((x ++ "__as_function")%string, (Skip, mexpr)) :: F) M mexpr v S'.
 Admitted.
+
+
+(* An expression will evaluate to the same value and store
+   after function definition list transformation.
+   This makes sense because the F transformation only adds function
+   definitions, and does not remove or overwrite any of them. *)
+Theorem expr_eval_eq_under_f_transformation :
+  forall S E G F M e v S',
+  exprevalR S E G F M e v S' ->
+    exprevalR S E G (transform_macros_F_e F M e) M e v S'.
+Proof.
+  intros.
+  induction H.
+  + (* Num z *)
+    apply E_Num.
+  + (* X x (local var) *)
+    apply E_X_Local with l.
+    * apply H.
+    * apply H0.
+  + (* X x (global var) *)
+    apply E_X_Global with l.
+    * apply H.
+    * apply H0.
+    * apply H1.
+  + (* ParenExpr e *)
+    apply E_ParenExpr.
+    simpl. apply IHexprevalR.
+  + (* UnExpr uo e *)
+    apply E_UnExpr.
+    simpl. apply IHexprevalR.
+  + (* BinExpr bo e1 e2 *)
+    apply E_BinExpr with S S'.
+    1, 2: simpl.
+    (* Here's the tricky part... *)
+    * admit.
+    * admit.
+  + (* Assign x e *)
+    apply E_Assign_Success.
+    * simpl. apply IHexprevalR.
+    * apply H0.
+  + (* CallOrInvocation x es (function call) *)
+    apply E_FunctionCall with fstmt fexpr S'.
+    * simpl. rewrite H. apply H.
+    * admit.
+    * admit.
+  + (* CallOrInvocation x es (macro invocation ) *)
+    apply E_MacroInvocation with mexpr.
+    * apply H.
+    * simpl. destruct (definition F x).
+      - apply H0.
+      - rewrite H.
+        destruct (has_side_effects mexpr).
+        ++ destruct (get_dynamic_vars mexpr).
+           ** apply H0.
+           ** apply H0.
+        ++ destruct (has_side_effects mexpr).
+          ** destruct (get_dynamic_vars mexpr).
+            -- apply expr_eval_same_under_unique_names. apply H0.
+            -- apply H0.
+          ** destruct (get_dynamic_vars mexpr).
+            -- apply expr_eval_same_under_unique_names. apply H0.
+            -- apply H0.
+Admitted.
+
 
 (* This lemma asserts that if two operands of a binary expression can
    be successfully transformed, then their transformed function
@@ -57,34 +93,38 @@ Admitted.
    because the function names we generate will all be unique, but we
    need this lemma to assist in the Coq proof. *)
 Lemma eval_same_under_joined_Fs :
-  forall S E F M S' bo e1 e2 v1 v2 S'',
-  exprevalR S E (transform_macros_F_e F M e1) M (transform_macros_e F M e1) v1 S' ->
-  exprevalR S' E (transform_macros_F_e F M e2) M (transform_macros_e F M e2) v2 S'' ->
-  exprevalR S E (transform_macros_F_e F M (BinExpr bo e1 e2)) M
+  forall S E G F M S' bo e1 e2 v1 v2 S'',
+  exprevalR S E G (transform_macros_F_e F M e1) M (transform_macros_e F M e1) v1 S' ->
+  exprevalR S' E G (transform_macros_F_e F M e2) M (transform_macros_e F M e2) v2 S'' ->
+  exprevalR S E G (transform_macros_F_e F M (BinExpr bo e1 e2)) M
   (transform_macros_e F M e1) v1 S'
   /\
-  exprevalR S' E (transform_macros_F_e F M (BinExpr bo e1 e2)) M
+  exprevalR S' E G (transform_macros_F_e F M (BinExpr bo e1 e2)) M
   (transform_macros_e F M e2) v2 S''.
 Admitted.
-
 
 (* Transforming an expression involving macros to one in which
    transformable macros have been converted to functions results
    in an expression that evaluates to the same value and state; i.e.,
    the transformation is sound. *)
 Theorem transform_macros_expressions_sound :
-  forall S E F M e v S',
-  exprevalR S E F M e v S' ->
-  exprevalR S E (transform_macros_F_e F M e) (transform_macros_M_e F M e) (transform_macros_e F M e) v S'.
+  forall S E G F M e v S',
+  exprevalR S E G F M e v S' ->
+  exprevalR S E G (transform_macros_F_e F M e) (transform_macros_M_e F M e) (transform_macros_e F M e) v S'.
 Proof.
   intros.
   induction H; unfold transform_macros_M_e in *.
   - (* Num z *)
     apply E_Num.
-  - (* X x *)
-    apply E_X_Success with l.
+  - (* X x (local var) *)
+    apply E_X_Local with l.
     + apply H.
     + apply H0.
+  - (* X x (global var) *)
+    apply E_X_Global with l.
+    + apply H.
+    + apply H0.
+    + apply H1.
   - (* ParenExpr e *)
     apply E_ParenExpr. apply IHexprevalR.
   - (* UnExpr uo e *)
@@ -110,7 +150,7 @@ Proof.
     unfold transform_macros_F_e.
     unfold transform_macros_e.
     rewrite H.
-    destruct (definition x F).
+    destruct (definition F x).
     + (* x is defined as a function
       (shouldn't happen so just use E_MacroInvocation *)
       apply E_MacroInvocation with mexpr. apply H. apply H0.
@@ -135,7 +175,7 @@ Proof.
                  Intuitively we know this will be true since all
                  the names in the transformed function list will be
                  unique, and we will only add names, never remove any. *)
-           ++ apply eval_same_under_unique_names. apply H0.
+           ++ apply expr_eval_same_under_unique_names. apply H0.
            (* x shares variables with the caller environment *)
         -- apply E_MacroInvocation with mexpr. apply H. apply H0.
 Qed.
@@ -147,19 +187,19 @@ Qed.
    evaluated as well. *)
 (* This may need some work to be made more conservative *)
 Lemma compound_statement_transformation_sound :
-  forall S E F M s0 rst stmts S' S'',
-  stmtevalR S E (transform_macros_F_s F M s0)
+  forall S E G F M s0 rst stmts S' S'',
+  stmtevalR S E G (transform_macros_F_s F M s0)
     (transform_macros_M_s F M s0)
     (transform_macros_s F M s0) S' ->
-  stmtevalR S' E
+  stmtevalR S' E G
     (transform_macros_F_s F M (CompoundStmt rst))
     (transform_macros_M_s F M (CompoundStmt rst))
     (transform_macros_s F M (CompoundStmt rst)) S'' ->
-  stmtevalR S E (transform_macros_F_s F M (CompoundStmt stmts))
+  stmtevalR S E G (transform_macros_F_s F M (CompoundStmt stmts))
   (transform_macros_M_s F M (CompoundStmt stmts))
   (transform_macros_s F M s0) S'
   /\
-  stmtevalR S' E (transform_macros_F_s F M (CompoundStmt stmts))
+  stmtevalR S' E G (transform_macros_F_s F M (CompoundStmt stmts))
   (transform_macros_M_s F M (CompoundStmt stmts))
   (CompoundStmt (map (transform_macros_s F M) rst)) S''.
 Admitted.
@@ -170,9 +210,9 @@ Admitted.
    in a statement that evaluates to the same state; i.e.,
    the transformation is sound. *)
 Theorem transform_macros_statements_sound :
-  forall S E F M s S',
-  stmtevalR S E F M s S' ->
-  stmtevalR S E (transform_macros_F_s F M s) (transform_macros_M_s F M s) (transform_macros_s F M s) S'.
+  forall S E G F M s S',
+  stmtevalR S E G F M s S' ->
+  stmtevalR S E G (transform_macros_F_s F M s) (transform_macros_M_s F M s) (transform_macros_s F M s) S'.
 Proof.
   intros.
   induction H.
@@ -227,16 +267,18 @@ Admitted.
 
 (* Expression evaluation does not change under the ID transformation *)
 Theorem transform_id_e_sound :
-  forall S E F M e v S',
-  exprevalR S E F M e v S' ->
-  exprevalR S E F M (transform_id_e e) v S'.
+  forall S E G F M e v S',
+  exprevalR S E G F M e v S' ->
+  exprevalR S E G F M (transform_id_e e) v S'.
 Proof.
   intros.
   induction H.
   - (* Num z *)
     apply E_Num.
-  - (* X x *)
-    apply E_X_Success with l. apply H. apply H0.
+  - (* X x (local var) *)
+    apply E_X_Local with l. apply H. apply H0.
+  - (* X x (global var) *)
+    apply E_X_Global with l. apply H. apply H0. apply H1.
   - (* ParenExpr e *)
     constructor. apply IHexprevalR.
   - (* UnExpr uo e *)
@@ -256,9 +298,9 @@ Qed.
 
 (* Statement evaluation does not change under the ID transformation *)
 Theorem transform_id_s_sound :
-  forall S E F M s S',
-  stmtevalR S E F M s S' ->
-  stmtevalR S E F M (transform_id_s s) S'.
+  forall S E G F M s S',
+  stmtevalR S E G F M s S' ->
+  stmtevalR S E G F M (transform_id_s s) S'.
 Proof.
   intros.
   induction H.
