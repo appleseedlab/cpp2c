36bc73078d3e7bd44253d99e1ef8f519910ea941
Brent Pappas <pappasbrent@gmail.com>
2022-01-31 12:53:56 -0500

    Proof of soundness for transformation of object-like macros without side-effects, shared variables in the caller environment, or nested macro invocations

--- proof_of_soundness/Theorems.v
+++ proof_of_soundness/Theorems.v
@@ -681,7 +681,7 @@ Proof.
       assert ((params, fstmt, fexpr) = (params0, fstmt0, fexpr0)).
       { apply StringMapFacts.MapsTo_fun with F x; auto. }
       inversion_clear H2. auto.
-  - intros. inversion_clear H1. apply StringMap_mapsto_in in m. contradiction.
+  - intros. inversion_clear H0. apply StringMap_mapsto_in in m. contradiction.
   - intros. inversion_clear H1. constructor; auto.
 Qed.
 
@@ -714,7 +714,7 @@ Proof.
       assert ((params, fstmt, fexpr) = (params0, fstmt0, fexpr0)).
       { apply StringMapFacts.MapsTo_fun with F x; auto. }
       inversion_clear H2. auto.
-  - intros. inversion_clear H1. apply StringMap_mapsto_in in m. contradiction.
+  - intros. inversion_clear H0. apply StringMap_mapsto_in in m. contradiction.
   - intros. inversion_clear H1. constructor; auto.
 Qed.
 
@@ -957,254 +957,6 @@ Proof.
 Qed.
 
 
-Lemma MSub_num_num : forall p e z ef,
-  MSub p e (Num z) ef ->
-  ef = Num z.
-Proof.
-  intros. inversion H; auto. Qed.
-
-
-Lemma macrosubst_num_num : forall ps es z ef,
-  MacroSubst ps es (Num z) ef ->
-  ef = Num z.
-Proof.
-  intros. remember (Num z) as e. induction H; subst; auto.
-  apply MSub_num_num in H. subst ef.
-  apply IHMacroSubst; auto.
-Qed.
-
-
-Inductive ExprNoNamesIn : expr -> list string -> Prop :=
-  | NNI_Num : forall z ps,
-    ExprNoNamesIn (Num z) ps
-  | NNI_Var : forall x ps,
-    ~ In x ps ->
-    ExprNoNamesIn (Var x) ps
-  | NNI_ParenExpr : forall e0 ps,
-    ExprNoNamesIn e0 ps ->
-    ExprNoNamesIn (ParenExpr e0) ps
-  | NNI_UnExpr : forall uo e0 ps,
-    ExprNoNamesIn e0 ps ->
-    ExprNoNamesIn (UnExpr uo e0) ps
-  | NNI_BinExpr : forall bo e1 e2 ps,
-    ExprNoNamesIn e1 ps ->
-    ExprNoNamesIn e2 ps ->
-    ExprNoNamesIn (BinExpr bo e1 e2) ps
-  | NNI_Assign : forall x e0 ps,
-    ~ In x ps ->
-    ExprNoNamesIn e0 ps ->
-    ExprNoNamesIn (Assign x e0) ps
-  | NNI_CallOrInvocation : forall x es ps,
-    ~ In x ps ->
-    ExprNoNamesInArgs es ps ->
-    ExprNoNamesIn (CallOrInvocation x es) ps
-with ExprNoNamesInArgs : list expr -> list string -> Prop :=
-  | NNIA_nil : forall ps,
-    ExprNoNamesInArgs nil ps
-  | NNIA_cons: forall e es ps,
-    ExprNoNamesIn e ps ->
-    ExprNoNamesInArgs (e::es) ps.
-
-
-Definition unop_eq (uo1 : unop) (uo2 : unop) : bool :=
-  match uo1 with
-  | Positive => match uo2 with
-    | Positive => true
-    | Negative => false
-    end
-
-  | Negative => match uo2 with
-    | Positive => false
-    | Negative => true
-    end
-end.
-
-
-Lemma unop_eq_refl : forall uo,
-  unop_eq uo uo = true.
-Proof.
-  intros. induction uo; auto. Qed.
-
-
-Definition binop_eq (bo1 : binop) (bo2 : binop) : bool :=
-  match bo1 with
-  | Plus => match bo2 with
-    | Plus => true
-    | Sub => false
-    | Mul => false
-    | Div => false
-  end
-
-  | Sub => match bo2 with
-    | Plus => false
-    | Sub => true
-    | Mul => false
-    | Div => false
-  end
-
-  | Mul => match bo2 with
-    | Plus => false
-    | Sub => false
-    | Mul => true
-    | Div => false
-  end
-
-  | Div => match bo2 with
-    | Plus => false
-    | Sub => false
-    | Mul => false
-    | Div => true
-  end
-end.
-
-
-(* This is just syntactically equal *)
-Fixpoint expr_equal (e1: expr) (e2: expr) : Prop :=
-  match e1 with
-  | Num z1 => match e2 with
-    | Num z2 => if (z1 =? z2)%Z then True else False
-    | _ => False
-    end
-
-  | Var x => match e2 with
-    | Var y => if (x =? y)%string then True else False
-    | _ => False
-    end
-
-  | ParenExpr e10 => match e2 with
-    | ParenExpr e20 => expr_equal e10 e20
-    | _ => False
-    end
-
-  | UnExpr uo1 e10 => match e2 with
-    | UnExpr uo2 e20 => if (unop_eq uo1 uo2)
-      then expr_equal e10 e20
-      else False
-    | _ => False
-    end
-
-  | BinExpr bo1 e11 e12 => match e2 with
-    | BinExpr bo2 e21 e22 => if (binop_eq bo1 bo2) then
-      expr_equal e11 e21 /\ expr_equal e12 e22
-      else False
-    | _ => False
-    end
-
-  | Assign x e10 => match e2 with
-    | Assign y e20 => if (x =? y)%string then
-      expr_equal e10 e20
-      else False
-    | _ => False
-    end
-
-  (* FIXME *)
-  | CallOrInvocation x es => False
-end.
-
-
-Lemma binop_eq_refl : forall bo,
-  binop_eq bo bo = true.
-Proof.
-  intros. induction bo; auto. Qed.
-
-
-
-Scheme MacroSubst_EvalArgs := Induction for MacroSubst Sort Prop
-with EvalArgs_MacroSubst := Induction for EvalArgs Sort Prop.
-
-
-
-Lemma no_side_effects_no_shared_vars_with_caller_evalargs_macrosubst :
-  forall mexpr,
-  (* The macro body must not have side-effects *)
-  ~ ExprHasSideEffects mexpr ->
-  forall F M,
-  (* The macro must not contain a macro invocation *)
-  NoMacroInvocations mexpr F M->
-  forall Ecaller,
-  (* The macro body must not rely on variables from the caller's scope.
-     Global variables, however, are allowed. *)
-  NoVarsInEnvironment mexpr Ecaller ->
-  forall es,
-  Forall (fun e => ~ExprHasSideEffects e) es ->
-  (* The macro arguments must not contain a macro invocation,
-     but they may contain variables from the caller's environment *)
-  NoMacroInvocationsArgs es F M ->
-  forall S G psf vs S' Ef Sargs l,
-  (* The arguments can be evaluated to create a function environment *)
-  EvalArgs S Ecaller G F M psf es vs S' Ef Sargs l ->
-  (* We will prove that S = S' since there are no side-effects.
-     S' and Sargs must be distinct so that we don't overwrite pre-existing
-     L-values *)
-  NatMapProperties.Disjoint S' Sargs ->
-  forall v S'''0,
-  (* The function body can be evaluated using call-by-value *)
-  EvalExpr (NatMapProperties.update S' Sargs) Ef G F M mexpr v S'''0 ->
-  forall psm ef,
-  (* The arguments can be substituted into the expression body *)
-  MacroSubst psm es mexpr ef ->
-  forall v0 S''',
-  (* The expression can be evaluated using call-by-name *)
-  EvalExpr S Ecaller G F M ef v0 S''' ->
-  (* The result of evaluation should be equal *)
-  v = v0.
-Proof.
-
-
-  (* Attempt 4: Induction over mexpr followed by case-analysis on es *)
-  induction mexpr.
-  -
-    intros. apply macrosubst_num_num in H7. subst ef.
-    inversion H6. inversion H8. subst. auto.
-  -
-    destruct es.
-    + admit.
-    + inversion_clear H1. intro. inversion_clear H1.
-      intro. inversion_clear H1. intros.
-      inversion H9. subst. inversion H1. subst.
-      induction e.
-      inversion H14. subst. apply macrosubst_num_num in H17. subst ef.
-      inversion H10. inversion H13. subst.
-
-
-  (* (* Attempt 3: Induction over vs *)
-  induction vs.
-  - admit.
-  - intros. eapply IHvs; eauto.
-    (* Gets close... *) *)
-
-
-  (* (* Attempt 2: Induction over es *)
-  induction es.
-  - (* Easy*) admit.
-  - intros. eapply IHes; eauto. inversion_clear H4.
-    inversion_clear H7.
-    (* Can't prove 3 and 4 *) *)
-  
-
-  (*
-  (* Attempt 1: Induction over the argument evaluation *)
-  intros until l. intro. assert (S=S').
-  { apply no_side_effects_no_store_change_evalargs_eq in H4; auto. }
-  subst S'.
-  
-  induction H4.
-  - intros. (* Should be easy *) admit.
-  - intros. apply IHEvalArgs with S'''0 psm ef S'''; auto.
-
-  intros until ef. intros H4. induction H4.
-  - intros. apply no_side_effects_no_shared_vars_with_caller_evalargs_macrosubst_nil with
-    mexpr F M Ecaller mexpr S G S''' S' Ef Sargs l S'''0; auto.
-    + constructor.
-    + inversion H5. subst. auto.
-  (* TODO: Induction on e and mexpr? I think that could work... *)
-  - inversion_clear H2. induction e, mexpr;
-    try (simpl in H6; contradiction); try (simpl in H; contradiction).
-    + admit.
-    + admit. *)
-Abort.
-
-
 Theorem transform_expr_sound_mut_v_s : forall M F e F' e',
   TransformExpr M F e F' e' ->
   (* The proof will not work if these variables were introduced sooner.
@@ -1215,26 +967,26 @@ Theorem transform_expr_sound_mut_v_s : forall M F e F' e',
     v1 = v2 /\ S'1 = S'2.
 Proof.
   apply (TransformExpr_mut
-    (* TransformExpr *)
+    (* Induction rule for TransformExpr *)
     (fun M F e F' e' (h : TransformExpr M F e F' e') =>
     forall S E G v1 v2 S'1 S'2,
       EvalExpr S E G F M e v1 S'1 ->
       EvalExpr S E G F' M e' v2 S'2 ->
       v1 = v2 /\ S'1 = S'2)
-    (* TransformArgs *)
+    (* Induction rule for TransformArgs *)
     (fun M F es F' es' (h : TransformArgs M F es F' es') =>
     forall S Ecaller G ps1 vs1 vs2 S'1 S'2 Ef1 Ef2 Sargs1 Sargs2 l1 l2,
       EvalArgs S Ecaller G F M ps1 es vs1 S'1 Ef1 Sargs1 l1 ->
       EvalArgs S Ecaller G F' M ps1 es' vs2 S'2 Ef2 Sargs2 l2 ->
       l1 = l2 /\ Ef1 = Ef2 /\ Sargs1 = Sargs2 /\ vs1 = vs2 /\ S'1 = S'2)
-    (* TransformStmt *)
+    (* Induction rule for TransformStmt *)
     (fun M F stmt F' stmt' (h : TransformStmt M F stmt F' stmt') =>
     forall S E G S'1 S'2,
       EvalStmt S E G F M stmt S'1 ->
       EvalStmt S E G F' M stmt' S'2 ->
       S'1 = S'2
       )); intros; auto.
-  -
+  - (* Num *)
     inversion H. inversion H0; subst. auto.
   -
     inversion H; subst.
@@ -1250,18 +1002,22 @@ Proof.
         rewrite H5 in H3. inversion H3; subst.
         apply NatMapFacts.find_mapsto_iff in H9, H12.
         rewrite H12 in H9. inversion H9. auto.
-  -
+
+  - (* ParenExpr *)
     inversion_clear H0. inversion_clear H1.
     destruct H with S E G v1 v2 S'1 S'2; auto.
-  -
+
+  - (* UnExpr *)
     inversion_clear H0. inversion_clear H1.
     destruct H with S E G v v0 S'1 S'2; auto.
     subst. auto.
-  -
+
+  - (* BinExpr *)
     inversion_clear H1. inversion_clear H2.
     destruct H with S E G v0 v4 S' S'0; auto.
     subst. destruct H0 with S'0 E G v3 v5 S'1 S'2; subst; auto.
-  -
+
+  - (* Assign *)
     inversion H0; subst.
     + inversion H1; subst.
       * apply StringMapFacts.find_mapsto_iff in H4, H5.
@@ -1275,9 +1031,9 @@ Proof.
         rewrite H7 in H5. inversion H5; subst.
         destruct H with S E G v1 v2 S' S'0; auto.
         subst. auto.
-  -
+  - (* Function call *)
     inversion_clear H2. inversion_clear H3.
-    + 
+    +
       assert (StringMap.MapsTo x (params, fstmt', fexpr') F').
       { subst. apply StringMapFacts.add_mapsto_iff. left. auto. }
       apply StringMapFacts.find_mapsto_iff in H15, H3.
@@ -1292,65 +1048,94 @@ Proof.
       apply NatMap_Equal_eq_iff in H14, H24. subst. auto.
     + apply StringMap_mapsto_in in H2. contradiction.
     + apply StringMap_mapsto_in in H4. contradiction.
-  -
-    inversion H1. inversion H2.
+
+  - (* Object like macro without side-effects, shared variables with the caller environment,
+       or nested macro invocations *)
+    inversion H0. inversion H1.
     + subst. apply StringMap_mapsto_in in m. contradiction.
     + subst. apply StringMap_mapsto_in in m. contradiction.
-    + inversion H2.
-      subst S0 E0 G0 F0 M0 x0 es0 v1 S'1.
-      subst S1 E1 G1 F1 M1 x1 es1 v2 S'''''.
-      * (* This is it *)
-        
-        (* No side-effects in function *)
+    + inversion H1.
+      subst S0 E0 G0 F0 M0 x0 es v1 S'1.
+      subst S1 E1 G1 F1 M1 x1 es0 v2 S'''''.
+      * (* Proof of equivalence *)
+
+        (* Prove that the macro body does not change after transformation *)
         assert (mexpr = mexpr').
         { apply nomacroinvocations_transformexpr_no_change with F M F'; auto. }
         subst mexpr'.
-        assert ( (params1, fstmt, fexpr) = (params, Skip, mexpr) ).
+
+        (* No side-effects in function *)
+
+        (* Prove that the definition that we know the function maps to
+           is the same one that we added during the transformation *)
+        assert ( (params1, fstmt, fexpr) = (nil, Skip, mexpr) ).
         { apply StringMapFacts.MapsTo_fun with F' fname; auto.
           rewrite e. apply StringMapFacts.add_mapsto_iff. left. auto. }
-        inversion H3. subst params1 fstmt fexpr. clear H3.
-        assert (Forall (fun e : expr => ~ ExprHasSideEffects e) es').
-        { apply no_side_effects_Forall_no_side_effects with
-          es M F F'; auto. }
-        assert (S = S'0).
-        { apply no_side_effects_no_store_change_evalargs_eq in H22; auto. }
-        subst S'0. apply skip_no_side_effects_eq in H27. subst S'''.
-        assert (S'' = S'''').
-        { apply no_side_effects_no_store_change_eq in H33; auto. }
+        inversion H2. subst params1 fstmt fexpr. clear H2.
+
+        (* Add to the context all the premises we get from evaluating an
+           empty argument list *)
+        inversion H21. subst Sprev E G0 F0 M0 vs S'0 Ef Sargs l.
+
+        (* Prove that there are no side-effects caused by the skip statement *)
+        apply skip_no_side_effects_eq in H26. subst S'''.
+
+        (* Prove that there are no side-effects cause by the return expression *)
+        assert (S'' = S''''). { apply no_side_effects_no_store_change_eq in H32; auto. }
         subst S''''.
+
+        (* Prove that after freeing the memort allocated for function environment,
+           the store reverts back to its original state *)
         assert (NatMap.Equal S (NatMapProperties.restrict S'' S)).
-        { rewrite H26. apply NatMap_disjoint_restrict_Equal; auto. }
-        rewrite <- H4 in H36. apply NatMap_Equal_eq_iff in H36. rewrite H36.
-        
+        { rewrite H25. apply NatMap_disjoint_restrict_Equal; auto. }
+        rewrite <- H2 in H35. apply NatMap_Equal_eq_iff in H35. subst S'2.
+
         (* No side-effects in macro *)
-        assert (es = es').
-        { apply nomacroinvocationsargs_transformargs_no_change with F M F'; auto. }
-        subst es'.
+
+        (* Prove that the definition that we know the macro maps to
+           is the same one we asserted in the transformation *)
         assert ( (params, mexpr) = (params0, mexpr0) ).
         { apply StringMapFacts.MapsTo_fun with M x; auto. }
-        symmetry in H8. inversion H8. subst params0 mexpr0. clear H8.
+        inversion H3. subst params0 mexpr0. clear H3.
+
+        (* Prove that after macro substitution, the macro body still
+           does not have side-effects *)
         assert (~ ExprHasSideEffects ef).
-        (* TODO: Fix this; probably need to add some things as premises in transformation *)
-        { apply no_side_effects_macrosubst_no_side_effects with mexpr params es; auto. }
+        { apply no_side_effects_macrosubst_no_side_effects with mexpr params nil; auto. }
+
+        (* Prove that the macro invocation does not have side-effects *)
         assert (S = S').
-        { apply no_side_effects_no_store_change_eq in H16; auto; auto. }
-        subst S'. apply NatMap_Equal_eq_iff in H4.
-        assert (NoVarsInEnvironment mexpr E).
-        { apply n2 with S G v S; auto. }
+        { apply no_side_effects_no_store_change_eq in H15; auto; auto. }
+        subst S'.
+
+        (* Use one of our premises to assert that the macro does not contain
+           variables from the caller environment *)
+        (* NOTE: We should probably add the environment to the transformation
+           relation so that we don't have to use such a generous premise *)
+        assert (NoVarsInEnvironment mexpr Ecaller).
+        { apply n1 with S G v S; auto. }
         split.
-        -- admit.
+        --
+          (* Prove that the expression will result in the same value
+             under call-by-name and call-by-value *)
+          apply no_side_effects_no_shared_vars_with_caller_evalargs_macrosubst_nil with
+            mexpr F' M' Ecaller mexpr S G S S (StringMap.empty nat) (NatMap.empty Z) 0 S'';
+              auto.
+            ++ (* Prove that the macro body does contain a macro invocation *)
+               subst M'. apply nomacroinvocations_remove_nomacroinvocations.
+               apply nomacroinvocations_transformexpr_nomacroinvocations with F mexpr; auto.
+            ++ constructor.
+            ++ (* Prove that macro expression evaluation works the same
+                  under an updated function table *)
+               inversion H12. subst ef mexpr0 params.
+               subst F'. apply evalexpr_notin_F_add_evalexpr; auto.
+            ++ constructor.
+            ++ (* Prove that function expression evaluation works the same
+                  under an updated macro table *)
+               subst S'' M'. apply nomacroinvocations_remove_evalexpr; auto.
+               apply nomacroinvocations_transformexpr_nomacroinvocations with F mexpr; auto.
         -- auto.
-        
-        (* Issues:
-           1) Proving value equivalence
-           2) Coq doesn't treatin FMap.Equal as the same thing as Logic.eq
-           
-           Solutions:
-           1) ???
-           2) Just use an axiom stating that Equal implies eq. *) 
-        
-        
-      * apply StringMap_mapsto_in in H19. contradiction. 
+      * apply StringMap_mapsto_in in H18. contradiction.
   - inversion H. inversion H0. subst. split; auto.
   - inversion H1. inversion H2.
     destruct H with S Ecaller G v v0 Snext Snext0; auto. subst. inversion H31. subst.
@@ -1358,6 +1143,13 @@ Proof.
     destruct H4. destruct H8. destruct H9. subst.
     split; auto.
   - inversion H. inversion H0. subst. auto.
-Abort.
+Qed.
 
 
+(* Issues:
+           1) Proving evaluation equivalence under call-by-name and call-by-value
+           2) Coq doesn't treat FMap.Equal as the same thing as Logic.eq
+
+           Solutions:
+           1) ???
+           2) Just use an axiom stating that Equal implies eq. *) 
