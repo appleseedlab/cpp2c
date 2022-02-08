9d3b08af0eef98a01802f37f4622e13c3950fdae
Brent Pappas <pappasbrent@gmail.com>
2022-02-08 13:04:42 -0500

    Add comments

--- proof_of_soundness/NoMacroInvocations.v
+++ proof_of_soundness/NoMacroInvocations.v
@@ -1,3 +1,9 @@
+(*  NoMacroInvocations.v
+    Definitions for relation in which the involved expression
+    does not invoke any macros from the given function table, and some
+    lemmas related to this relation.
+*)
+
 Require Import
   Coq.Strings.String.
 
@@ -8,44 +14,67 @@ From Cpp2C Require Import
   NoCallsFromFunctionTable
   Syntax.
 
-
+(*  Definition of the relation.
+    We need to pass a function table to the relation to check if 
+    body of a function contains a macro invocation *)
 Inductive ExprNoMacroInvocations : expr -> function_table -> macro_table -> Prop :=
   | NM_Num : forall z F M,
+    (*  Numerals cannot invoke macros *)
     ExprNoMacroInvocations (Num z) F M
   | NM_Var : forall x F M,
+    (*  Variables cannot invoke macros *)
     ExprNoMacroInvocations (Var x) F M
   | NM_ParenExpr : forall e0 F M,
+    (*  The inner expression does not contain an invocation *)
     ExprNoMacroInvocations e0 F M ->
     ExprNoMacroInvocations (ParenExpr e0) F M
   | NM_UnExpr : forall uo e0 F M,
+    (*  The inner expression does not contain an invocation *)
     ExprNoMacroInvocations e0 F M ->
     ExprNoMacroInvocations (UnExpr uo e0) F M
   | NM_Bin_Expr : forall bo e1 e2 F M,
+    (*  The left operand does not contain an invocation *)
     ExprNoMacroInvocations e1 F M ->
+    (*  The right opreand does not contain an invocation *)
     ExprNoMacroInvocations e2 F M ->
     ExprNoMacroInvocations (BinExpr bo e1 e2) F M
   | NM_Assign : forall x e0 F M,
+    (*  The inner expression does not contain an invocation *)
     ExprNoMacroInvocations e0 F M ->
     ExprNoMacroInvocations (Assign x e0) F M
   | NM_CallOrInvocation : forall x es F M params fstmt fexpr,
+    (*  The invocation is not to a macro *)
     ~ StringMap.In x M ->
+    (*  None of the arguments contain an invocation *)
     ExprListNoMacroInvocations es F M ->
+    (*  Retrieve the body of the function that is being called *)
     StringMap.MapsTo x (params, fstmt, fexpr) F ->
+    (*  TODO: Add another relation for asserting that a statement
+        does not contain a macro invocation. Right now we don't need
+        one since all statements are Skip statements *)
+    (*  The return expression of the invocation does not contain
+        any macro invocations *)
     ExprNoMacroInvocations fexpr F M ->
     ExprNoMacroInvocations (CallOrInvocation x es) F M
 with ExprListNoMacroInvocations : list expr -> function_table -> macro_table -> Prop :=
   | NM_ExprList_nil : forall F M,
+    (*  An empty expression list contains no invocations *)
     ExprListNoMacroInvocations nil F M
   | NM_ExprList_cons : forall e es F M,
+    (*  The head of the list does not contain any invocations*)
     ExprNoMacroInvocations e F M ->
+    (*  The tail of the list does not contain any invocations *)
     ExprListNoMacroInvocations es F M ->
     ExprListNoMacroInvocations (e::es) F M.
 
-
+(*  Custom induction scheme *)
 Scheme ExprNoMacroInvocations_mut := Induction for ExprNoMacroInvocations Sort Prop
 with ExprListNoMacroInvocations_mut := Induction for ExprListNoMacroInvocations Sort Prop.
 
 
+(*  If an expression does not contain any macro invocations from a given
+    macro table, then it does not contain any macro invocations from
+    any other macro table equal to the first *)
 Lemma M_Equal_ExprNoMacroInvocations : forall e F M_1,
   ExprNoMacroInvocations e F M_1 ->
   forall M_2,
@@ -66,6 +95,9 @@ Proof.
 Qed.
 
 
+(*  If an expression does not contain any macro invocations from a given
+    macro table, and we remove a macro from that table, then the expression
+    will not contain any macro invocations under the new table *)
 Lemma ExprNoMacroInvocations_remove_ExprNoMacroInvocations : forall e F M,
   ExprNoMacroInvocations e F M ->
   forall x,
@@ -85,6 +117,9 @@ Proof.
 Qed.
 
 
+(*  If an expression terminates under a given context and does not contain
+    any macro invocations, then it terminates under a different context
+    in which a mapping has been removed from the macro table *)
 Lemma EvalExprNoMacroInvocations_remove_EvalExpr : forall S E G F M e v S',
   EvalExpr S E G F M e v S' ->
   ExprNoMacroInvocations e F M ->
@@ -127,7 +162,10 @@ Proof.
 Qed.
 
 
-Lemma ExprNoMacroInvocations_remove_EvalExprList : forall Sprev Ecaller G F M ps es vs Snext Ef Sargs l ls,
+(*  If an expression list terminates under a given context and does not contain
+    any macro invocations, then it terminates under a different context
+    in which a mapping has been removed from the macro table *)
+Lemma ExprListNoMacroInvocations_remove_EvalExprList : forall Sprev Ecaller G F M ps es vs Snext Ef Sargs l ls,
   EvalExprList Sprev Ecaller G F M ps es vs Snext Ef Sargs l ls->
   ExprListNoMacroInvocations es F M ->
   forall x,
@@ -169,6 +207,11 @@ Proof.
 Qed.
 
 
+(*  If an expression does not contain any macro invocations from a given
+    macro table under a given function table, and we add to that
+    function table all the mappings of a function table that the
+    expression calls no functions from, then the expression
+    will not contain any macro invocations under the new function table *)
 Lemma ExprNoMacroInvocations_update_ExprNoCallFromFunctionTable_ExprNoMacroInvocations :
   forall e F M,
   ExprNoMacroInvocations e F M ->
@@ -202,7 +245,12 @@ Proof.
 Qed.
 
 
-Lemma ExprListNoMacroInvocations_update_ExprNoCallsFromFunctionTableExprList_ExprListNoMacroInvocations:
+(*  If an expression list does not contain any macro invocations from a given
+    macro table under a given function table, and we add to that
+    function table all the mappings of a function table that the
+    expression calls no functions from, then the expression list
+    will not contain any macro invocations under the new function table *)
+Lemma ExprListNoMacroInvocations_update_ExprListNoCallsFromFunctionTableExprList_ExprListNoMacroInvocations:
   forall es F M,
   ExprListNoMacroInvocations es F M ->
   forall F',
@@ -217,6 +265,9 @@ Proof.
 Qed.
 
 
+(*  If an expression does not contain any macro invocations, and we substitute
+    into it a single expression that also does not contain macro invocations,
+    then the final expression will also not contain macro invocations *)
 Lemma ExprNoMacroInvocations_MSub_ExprNoMacroInvocations : forall mexpr F M,
   ExprNoMacroInvocations mexpr F M ->
   forall e,
@@ -257,6 +308,9 @@ Proof.
 Qed.
 
 
+(*  If an expression does not contain any macro invocations, and we substitute
+    into it a mutiple expressions that also do not contain macro invocations,
+    then the final expression will also not contain macro invocations *)
 Lemma ExprNoMacroInvocations_NoMacroInvocationExprList_MacroSubst_ExprNoMacroInvocations : forall mexpr F M,
   ExprNoMacroInvocations mexpr F M ->
   forall es,
