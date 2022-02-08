16bbbd228f5cb4534768423118a90ce3604530f6
Brent Pappas <pappasbrent@gmail.com>
2022-02-08 13:26:03 -0500

    Add comments

--- proof_of_soundness/NoVarsInEnvironment.v
+++ proof_of_soundness/NoVarsInEnvironment.v
@@ -1,3 +1,7 @@
+(*  NoVarsInEnvironment.v
+    Definition of a relation in which a given expression contains no
+    variables in a given environment *)
+
 Require Import
   Coq.Lists.List
   Coq.Logic.Classical_Prop
@@ -8,95 +12,54 @@ From Cpp2C Require Import
   EvalRules
   Syntax.
 
-
+(*  Definition of the relation *)
 Inductive ExprNoVarsInEnvironment : expr -> environment -> Prop :=
   | NV_Num : forall z E,
+    (*  Numerals of course do not contain a variable *)
     ExprNoVarsInEnvironment (Num z) E
   | NV_Var : forall x E,
+    (*  The variable is not in the given environment *)
     ~ StringMap.In x E ->
     ExprNoVarsInEnvironment (Var x) E
   | NV_ParenExpr : forall e0 E,
+    (*  The inner expression does not contain any variable
+        in the given environment *)
     ExprNoVarsInEnvironment e0 E ->
     ExprNoVarsInEnvironment (ParenExpr e0) E
   | NV_UnExpr : forall uo e0 E,
+    (*  The inner expression does not contain any variable
+        in the given environment *)
     ExprNoVarsInEnvironment e0 E ->
     ExprNoVarsInEnvironment (UnExpr uo e0) E
   | NV_Bin_Expr : forall bo e1 e2 E,
+    (*  The left operand does not contain any variable
+        in the given environment *)
     ExprNoVarsInEnvironment e1 E ->
+    (*  The right operand does not contain any variable
+        in the given environment *)
     ExprNoVarsInEnvironment e2 E ->
     ExprNoVarsInEnvironment (BinExpr bo e1 e2) E
   | NV_Assign : forall x e0 E,
+    (*  The LHS of the assignment is not in the given environment *)
     ~ StringMap.In x E ->
+    (*  The inner expression does not contain any variable
+        in the given environment *)
     ExprNoVarsInEnvironment e0 E ->
     ExprNoVarsInEnvironment (Assign x e0) E
   | NV_CallOrInvocation : forall x es E,
-    ~ StringMap.In x E ->
+    (*  None of the arguments contain a variable
+        in the given environment *)
     ExprListNoVarsInEnvironment es E ->
     ExprNoVarsInEnvironment (CallOrInvocation x es) E
 with ExprListNoVarsInEnvironment : list expr -> environment -> Prop :=
   | NV_ExprList_nil : forall E,
+    (*  An empty expression list does not contain a variable *)
     ExprListNoVarsInEnvironment nil E
   | NV_ExprList_cons : forall e es E,
+    (*  The head of the list does not contain a variable in the
+        given environment *)
     ExprNoVarsInEnvironment e E ->
+    (*  The tail of the list does not contasin a variable in the
+        given environment *)
     ExprListNoVarsInEnvironment es E ->
     ExprListNoVarsInEnvironment (e::es) E.
-
-
-Scheme ExprNoVarsInEnvironment_mut := Induction for ExprNoVarsInEnvironment Sort Prop
-with ExprListNoVarsInEnvironment_mut := Induction for ExprListNoVarsInEnvironment Sort Prop.
-
-
-Lemma ExprNoVarsInEnvironment_MSub_ExprNoVarsInEnvironment : forall mexpr E,
-  ExprNoVarsInEnvironment mexpr E ->
-  forall e,
-  ExprNoVarsInEnvironment e E ->
-  forall p ef,
-  MSub p e mexpr ef ->
-  ExprNoVarsInEnvironment ef E.
-Proof.
-  apply (ExprNoVarsInEnvironment_mut
-    (fun mexpr E (h : ExprNoVarsInEnvironment mexpr E) =>
-      forall e,
-      ExprNoVarsInEnvironment e E ->
-      forall p ef,
-      MSub p e mexpr ef ->
-      ExprNoVarsInEnvironment ef E)
-    (fun es E (h : ExprListNoVarsInEnvironment es E) =>
-      forall e,
-      ExprNoVarsInEnvironment e E ->
-      forall p es',
-      MSubList p e es es' ->
-      ExprListNoVarsInEnvironment es' E)); intros; try constructor; auto.
-  - inversion_clear H0; constructor.
-  - inversion H0.
-    + subst; auto.
-    + subst. constructor; auto.
-  - inversion_clear H1. constructor. eapply H; eauto.
-  - inversion_clear H1. constructor. eapply H; eauto.
-  - inversion_clear H2. constructor.
-    + eapply H; eauto.
-    + eapply H0; eauto.
-  - inversion H1. 
-    + subst. constructor. inversion H0; auto. eapply H; eauto.
-    + subst. constructor; auto. eapply H; eauto.
-  - inversion_clear H1. constructor; eauto.
-  - inversion_clear H0. constructor.
-  - inversion_clear H2. constructor.
-    + eapply H; eauto.
-    + eapply H0; eauto.
-Qed.
-
-
-Lemma ExprNoVarsInEnvironment_MacroSubst_ExprNoVarsInEnvironment : forall mexpr E,
-  ExprNoVarsInEnvironment mexpr E ->
-  forall es,
-  Forall (fun e => ExprNoVarsInEnvironment e E) es ->
-  forall ps ef,
-  MacroSubst ps es mexpr ef ->
-  ExprNoVarsInEnvironment ef E.
-Proof.
-  intros. induction H1.
-  - auto.
-  - inversion_clear H0. apply IHMacroSubst; auto.
-    apply ExprNoVarsInEnvironment_MSub_ExprNoVarsInEnvironment with mexpr e p; auto.
-Qed.
