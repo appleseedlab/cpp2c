62b1609f26be2e65c7f4a4fbc17b4c35eda99230
Brent Pappas <pappasbrent@gmail.com>
2022-03-01 13:31:29 -0500

    Add note on how THREE() gets transformed if body is enclosed in parentheses

--- transformation_tool/tests/object_like_and_function_like_macros_body_const_expr.c
+++ transformation_tool/tests/object_like_and_function_like_macros_body_const_expr.c
@@ -5,6 +5,8 @@ int main(void)
 {
     THREE();
     // Right now THREE() is not transformed, but should it be?
+    // If we change the definition of THREE() to (1 + 2), then this
+    // invocation will be transformed
     ONE + THREE();
     return 0;
 }
