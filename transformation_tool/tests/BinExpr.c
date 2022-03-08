f561e0b12bb9146e70866cfdb4d63ef2b0a9afc7
Brent Pappas <pappasbrent@gmail.com>
2022-03-08 13:01:47 -0500

    Add macro definitions that should not be transformed

--- transformation_tool/tests/BinExpr.c
+++ transformation_tool/tests/BinExpr.c
@@ -3,11 +3,22 @@
 #define BIN_EXPR_MULT 1 * 1
 #define BIN_EXPR_DIV 1 / 1
 
+#define M 1 +
+#define N 1
+
 int main()
 {
+    // Should transform
     BIN_EXPR_PLUS;
+    // Should transform
     BIN_EXPR_MINUS;
+    // Should transform
     BIN_EXPR_MULT;
+    // Should transform
     BIN_EXPR_DIV;
+
+    // Should not transform
+    M N;
+
     return 0;
 }
