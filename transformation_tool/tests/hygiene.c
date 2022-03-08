12ac6c920288bd9654c274fc884b7f21258044b3
Brent Pappas <pappasbrent@gmail.com>
2022-03-08 17:59:04 -0500

    Add more tests

--- transformation_tool/tests/hygiene.c
+++ transformation_tool/tests/hygiene.c
@@ -1,9 +1,29 @@
 #define ADD_UNHYGIENIC(a, b) a + b
 #define ADD_HYGIENIC(a, b) (a + b)
 
+#define M 0 + 5
+
 int main()
 {
+    // Should transform
     0 * ADD_HYGIENIC(2, 3);
+
+    // Should not transform
     0 * ADD_UNHYGIENIC(2, 3);
+
+    // Should transform
+    M;
+
+    // Should not transform
+    M * 1;
+
+    // Should only transform parenthesized expression
+    (M) * M * 1;
+    // (0 + 5) * 0 + 5 * 1
+
+    // Should only transform parenthesized expression
+    M * (M) * 1;
+    // 0 + 5 * (0 + 5) * 1
+
     return 0;
 }
