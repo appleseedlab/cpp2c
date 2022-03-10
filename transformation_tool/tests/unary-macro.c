9efaf606ed3bc6abffbfd18201020deb70936b34
Brent Pappas <pappasbrent@gmail.com>
2022-03-10 09:55:08 -0500

    Add tests for the ID macro

---
+++ transformation_tool/tests/unary-macro.c
@@ -0,0 +1,36 @@
+#define ID(x) x
+
+#define ADD_ONE(x) x + 1
+
+int x = 0;
+
+int foo() { return 0; }
+
+int main()
+{
+    // Should transform
+    ID(1);
+
+    // Should transform
+    ID(x);
+
+    // Should transform
+    ID((1));
+
+    // Should transform
+    ID(-1);
+
+    // Should transform
+    ID(1 + 1);
+
+    // Should not transform
+    ID(x = 2);
+
+    // Should not transform
+    ID(foo());
+
+    // Should not transform
+    ADD_ONE(1);
+
+    return 0;
+}
