8e0dd1de23928a89d8dde7f392400dbaf71c7da5
Brent Pappas <pappasbrent@gmail.com>
2022-03-14 12:49:57 -0400

    Format file.
    Add FOO macro.
    
    Add OUTER and INNER macros.

--- transformation_tool/tests/hygiene.c
+++ transformation_tool/tests/hygiene.c
@@ -1,11 +1,20 @@
+#define FOO 1 + 2 + 3
+
 #define ADD_UNHYGIENIC(a, b) a + b
 #define ADD_HYGIENIC(a, b) (a + b)
 
 #define M 0 + 5
 #define N 1 +
 
+#define OUTER INNER
+
+#define INNER 1
+
 int main()
 {
+
+    FOO;
+
     // Should transform
     0 * ADD_HYGIENIC(2, 3);
 
@@ -19,7 +28,7 @@ int main()
     M * 1;
 
     // Should not transform
-    M * M * 1;
+    M *M * 1;
     // 0 + 5 * 0 + 5 * 1
 
     // Should only transform parenthesized expression
@@ -27,15 +36,20 @@ int main()
     // (0 + 5) * 0 + 5 * 1
 
     // Should only transform parenthesized expression
-    M * (M) * 1;
+    M *(M)*1;
     // 0 + 5 * (0 + 5) * 1
 
     // Should transform both parenthesized expressions
-    (M) * (M) * 1;
+    (M) * (M)*1;
     // (0 + 5) * (0 + 5) * 1
 
     // Should not transform
     N N 1;
+    // 1 + 1 + 1
+
+    // Should not transform
+    OUTER;
+    // 1
 
     return 0;
 }
