7c36c0b83990925ef151b6956d8bc62147b2490c
Brent Pappas <pappasbrent@gmail.com>
2022-03-14 15:34:58 -0400

    Add static method to check if a statement or expression in the C subset has side-effects

--- transformation_tool/src/CSubsetHasSideEffects.h
+++ transformation_tool/src/CSubsetHasSideEffects.h
@@ -9,6 +9,8 @@ private:
     bool result = false;
 
 public:
+    CSubsetHasSideEffects(ASTContext *Ctx) : CSubsetVisitor(Ctx){};
+
     virtual void VisitAssign(const BinaryOperator *Assign)
     {
         result = true;
@@ -20,4 +22,18 @@ public:
     }
 
     bool getResult() { return result; }
+
+    static bool hasSideEffects(ASTContext *Ctx, const Stmt *S)
+    {
+        CSubsetHasSideEffects Checker(Ctx);
+        Checker.VisitStmt(S);
+        return Checker.getResult();
+    }
+
+    static bool hasSideEffects(ASTContext *Ctx, const Expr *E)
+    {
+        CSubsetHasSideEffects Checker(Ctx);
+        Checker.VisitExpr(E);
+        return Checker.getResult();
+    }
 };
