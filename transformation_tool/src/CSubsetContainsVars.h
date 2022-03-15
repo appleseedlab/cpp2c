04e92add73465fad5ac41c11b8b6d8f56386cc8d
Brent Pappas <pappasbrent@gmail.com>
2022-03-15 18:33:21 -0400

    Visitor to check if an expression contains a variable reference

---
+++ transformation_tool/src/CSubsetContainsVars.h
@@ -0,0 +1,26 @@
+#include "CSubsetVisitor.h"
+
+// Visitor for checking if a program, statement, or expression in the
+// Cpp2C C subset has local variables.
+class CSubsetContainsVars : public CSubsetVisitor
+{
+private:
+    bool result = false;
+
+public:
+    CSubsetContainsVars(ASTContext *Ctx) : CSubsetVisitor(Ctx){};
+
+    void VisitVar(const VarDecl *Var)
+    {
+        result = true;
+    }
+
+    bool getResult() { return result; }
+
+    static bool containsVars(ASTContext *Ctx, const Expr *E)
+    {
+        CSubsetContainsVars Checker(Ctx);
+        Checker.VisitExpr(E);
+        return Checker.getResult();
+    }
+};
