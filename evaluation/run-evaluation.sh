802efe7e89c715c4ca1bacc631bbd213952a13dd
Brent Pappas <pappasbrent@gmail.com>
2022-03-19 16:20:39 -0400

    Aggregate all program stats into single CSV file

--- evaluation/run-evaluation.sh
+++ evaluation/run-evaluation.sh
@@ -2,8 +2,10 @@
 
 # Runs the evaluation of Cpp2C
 
+STATS_DIR=stats
+
 # Clear the stats folder
-rm -fr stats/*
+rm -fr $STATS_DIR/*
 
 # Run the evaluation scripts
 for FN in $(ls evaluate-*.sh); do
@@ -16,11 +18,12 @@ for FN in $(ls evaluate-*.sh); do
 done
 
 # Aggregate the transformation data of each program into a single CSV file each
-for DIR in $(find stats/* -type d); do
+for DIR in $(find $STATS_DIR/* -type d); do
     python3 aggregate_program_stats.py $DIR > $DIR.csv
-    if [ $? = 1 ]; then
+    if [ $? != 0 ]; then
         echo "Error aggregating data for directory $DIR"
     fi
 done
 
 # Aggregate transformation data of all programs into a single CSV file
+python3 aggregate_all_stats.py > all_stats.csv
