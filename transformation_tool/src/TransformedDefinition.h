2ee676799d399f3b446dba97d0b87f1444feeb38
Brent Pappas <pappasbrent@gmail.com>
2022-03-20 17:33:02 -0400

    Finish definition of NewTransformedDefinition

--- transformation_tool/src/TransformedDefinition.h
+++ transformation_tool/src/TransformedDefinition.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "clang/Lex/Lexer.h"
+
 #include "MacroForest.h"
 
 using namespace std;
@@ -7,8 +9,6 @@ using namespace clang;
 
 struct TransformedDefinition
 {
-    // The name of the file that the original macro was defined in
-    string SourceFileName;
     // The name of the original macro that this transformation came from
     string OriginalMacroName;
     // Whether this transformation is to a variable or a function
@@ -19,7 +19,7 @@ struct TransformedDefinition
     // A vector of the types of the transformed function's arguments
     vector<string> ArgTypes;
     // The body of the transformed definition
-    string Body;
+    string InitializerOrDefinition;
     // The name used when emitting this definition
     string EmittedName;
 };
@@ -28,13 +28,40 @@ struct TransformedDefinition NewTransformedDefinition(
     ASTContext *Ctx, MacroForest::Node *Expansion, bool IsVar)
 {
     auto &SM = Ctx->getSourceManager();
+    auto &LO = Ctx->getLangOpts();
 
     struct TransformedDefinition TD;
-    TD.SourceFileName =
-        SM.getFilename(Expansion->SpellingRange.getBegin()).str();
     TD.OriginalMacroName = Expansion->Name;
     TD.IsVar = IsVar;
-    // TODO: Finish
+    assert(Expansion->Stmts.size() == 1);
+    TD.VarOrReturnType = getType(Ctx, *(Expansion->Stmts.begin()));
+    for (auto &&Arg : Expansion->Arguments)
+    {
+        // Note that we don't assert that the argument has only one stmt.
+        // This is because the argument may have been asserted multiple times.
+        string ArgType = getType(Ctx, *(Arg.Stmts.begin()));
+        TD.ArgTypes.push_back(ArgType);
+    }
+
+    // Generate the transformed body
+    SourceLocation MacroBodyBegin =
+        Expansion->MI->tokens().front().getLocation();
+    SourceLocation MacroBodyEnd =
+        Lexer::getLocForEndOfToken(
+            Expansion->DefinitionRange.getEnd(), 0, SM, LO);
+
+    SourceRange MacroBodyRange = SourceRange(MacroBodyBegin, MacroBodyEnd);
+    CharSourceRange MacroBodyCharRange =
+        CharSourceRange::getCharRange(MacroBodyRange);
+    string TransformedBody =
+        Lexer::getSourceText(MacroBodyCharRange, SM, LO).str();
+
+    string InitializerOrDefinition =
+        (IsVar
+             ? " = " + TransformedBody + ";"
+             : " { return " + TransformedBody + "; }");
+
+    TD.InitializerOrDefinition = InitializerOrDefinition;
 
     return TD;
 }
