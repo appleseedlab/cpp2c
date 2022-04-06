dce75af656993b9165b172f92e1131565fa3ec69
Brent Pappas <pappasbrent@gmail.com>
2022-04-06 18:22:25 -0400

    Remove script for removing evaluation artifacts

--- evaluation/reset-evaluation.sh
+++
@@ -1,3 +0,0 @@
-rm -fr stats
-rm -fr gzip-1.10 bc-1.07 lua-5.4.4 lua-5.4.4-tests redis-6.2.6 remind-03.04.02 tiny-HMAC-c-93bdfa8114a32cf25ba70cb1f0957d3bf0f180af tiny-lint-c-4de8b64c97fda3117a7ddc9895a30adb97fbae97 tiny-regex-c-2d306a5a71128853d18292e8bb85c8e745fbc9d0
-rm -fr *.zip *.tar.gz
