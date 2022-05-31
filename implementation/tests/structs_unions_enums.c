a8fca36ce3b987c2ccc262a9fc30fcc5982e9ff7
Brent Pappas <pappasbrent@gmail.com>
2022-05-31 10:01:51 -0400

    Add test cases for transforming macros with anonynous user-defined types in their signatures (structs/unions/enums)

--- implementation/tests/structs_unions_enums.c
+++ implementation/tests/structs_unions_enums.c
@@ -1,30 +1,41 @@
 #define GET_X_STATIC(su) su.x
 #define GET_X_PTR(su) su->x
+#define GET_S_STATIC(u) u.s
 
-struct S
+typedef struct S
 {
     int x;
-};
+} S;
 
 union U
 {
     int x;
     int y;
+    S s;
 };
 
-struct __attribute__((annotate("CPP2C"))) X;
+// This should be made not anonymous
+typedef enum
+{
+    Y = 0
+} M;
 
-int x __attribute__((annotate("CPP2C")));
-
-int f() __attribute__((annotate("CPP2C")));
+M f(M m) { return m; }
+#define CALL_F_WITH(N) f(N)
 
 int main(int argc, char const *argv[])
 {
-    struct S s;
-    union U u;
+    S s;
     GET_X_STATIC(s);
-    GET_X_STATIC(u);
     GET_X_PTR((&s));
+
+    union U u;
+    GET_X_STATIC(u);
+    GET_S_STATIC(u);
+
+    M m;
+    CALL_F_WITH(m);
+
 #undef GET_X_PTR
 #define GET_X_PTR(su) su->x
     GET_X_PTR((&s));
