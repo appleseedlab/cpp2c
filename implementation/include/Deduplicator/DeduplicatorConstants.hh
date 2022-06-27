882e159f5585819d8da66239276baeac492f212e
Brent Pappas <pappasbrent@gmail.com>
2022-06-26 20:46:51 -0400

    Move JSON keys specific to deduplicator a header file

---
+++ implementation/include/Deduplicator/DeduplicatorConstants.hh
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace Deduplicator
+{
+    namespace Keys
+    {
+        const std::string
+            CANONICAL = "canonical",
+            UNIQUE_TRANSFORMED_INVOCATIONS = "unique transformed invocations";
+    } // namespace Keys
+} // namespace Deduplicator
