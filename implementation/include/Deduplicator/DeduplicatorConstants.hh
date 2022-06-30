e86324ebbda373a802dd6f28d4a287f6c96c66d0
Brent Pappas <pappasbrent@gmail.com>
2022-06-30 10:41:04 -0400

    Add key for transformations to be kept across runs, and remove canonical key

--- implementation/include/Deduplicator/DeduplicatorConstants.hh
+++ implementation/include/Deduplicator/DeduplicatorConstants.hh
@@ -7,7 +7,7 @@ namespace Deduplicator
     namespace Keys
     {
         const std::string
-            CANONICAL = "canonical",
+            KEEP_ACROSS_RUNS = "keep across runs",
             UNIQUE_TRANSFORMED_INVOCATIONS = "unique transformed invocations";
     } // namespace Keys
 } // namespace Deduplicator
