a17466250f38e8b63830724cea64ea8f0be34a63
Brent Pappas <pappasbrent@gmail.com>
2022-07-04 23:12:53 -0400

    Count untransformed invocations
    Move some printing around

--- evaluation/run_evaluation.py
+++ evaluation/run_evaluation.py
@@ -31,6 +31,14 @@ PARAMETER_SIDE_EFFECTS = 'Parameter side-effects'
 UNSUPPORTED_CONSTRUCT = 'Unsupported construct'
 
 
+def macro_hash_from_annotation(annotation: Dict) -> str:
+    '''Given a Cpp2C annotation, returns its macro hash'''
+    return (annotation['macro name'] + ";" +
+            annotation['macro type'] + ";" +
+            annotation['macro definition realpath'] + ";" +
+            str(annotation['macro definition number']))
+
+
 def three_num(data):
     """90, 95, and 99th percentiles."""
     # Taken from Wikipedia
@@ -57,7 +65,7 @@ def twenty_num(data):
 
 
 def is_system_header_path(path: str) -> bool:
-    return '/usr/include' in path or '/usr/lib' in path
+    return '/usr/include' in path or '/usr/lib' in path or '<scratch space>' in path or path == ''
 
 
 # Disable numpy text wrapping
@@ -100,8 +108,7 @@ def main():
 
         # Perform a dry run to count the number of unique expansions of
         # macros defined in the program
-        hashes_of_macros_defined_in_program: Set[str] = set()
-        macro_hashes_to_expansion_spelling_locations: Dict[str, Set[str]] = defaultdict(
+        mhash_to_expansion_spellings_locs: Dict[str, Set[str]] = defaultdict(
             set)
         for cc in compile_commands:
             cmd = compile_command.cpp2c_command_from_compile_command(cc, [
@@ -118,40 +125,44 @@ def main():
 
             # Check for macro definitions and expansions
             for line in cp.stderr.splitlines():
-                if line.startswith(f'CPP2C:{MACRO_DEFINITION}'):
+                if line.startswith(f'CPP2C:{MACRO_EXPANSION}'):
                     _, mhash, spelling_location = line.split(',')
+                    # fn, ln, cl = spelling_location.split(':')
+                    # Only consider macros which were expanded in c or h files
+                    # if fn.endswith('.c') or fn.endswith('.h'):
+                    # Only consider macros which were expanded at least once
+                    # and defined in the source code
                     mname, mtype, mdef_path, mdef_num = mhash.split(';')
-                    # Only consider macros which were defined in the source code,
-                    # not those defined a system header or on the command line
-                    if (not is_system_header_path(mdef_path) and
-                            mdef_path != ''):
-                        hashes_of_macros_defined_in_program.add(mhash)
-
-                elif line.startswith(f'CPP2C:{MACRO_EXPANSION}'):
-                    _, mhash, spelling_location = line.split(',')
-                    if mhash in hashes_of_macros_defined_in_program:
-                        macro_hashes_to_expansion_spelling_locations[mhash].add(
+                    if not is_system_header_path(mdef_path):
+                        mhash_to_expansion_spellings_locs[mhash].add(
                             spelling_location)
 
         print(f'## {evaluation_program.name}')
 
-        print(
-            f'unique macro definitions: {len(hashes_of_macros_defined_in_program)}')
-        print(
-            f'    olms: {len([h for h in hashes_of_macros_defined_in_program if OBJECT_LIKE in h])}')
-        print(
-            f'    flms: {len([h for h in hashes_of_macros_defined_in_program if FUNCTION_LIKE in h])}')
+        expanded_source_macros = mhash_to_expansion_spellings_locs.keys()
 
         print(
-            f'unique expansions of source macros: {sum(len(vs) for vs in macro_hashes_to_expansion_spelling_locations.values())}')
+            f'unique expanded source macro definitions: {len(expanded_source_macros)}')
         print(
-            f'    olms: {sum(len(vs) for h, vs in macro_hashes_to_expansion_spelling_locations.items() if OBJECT_LIKE in h)}')
+            f'    olms: {len([h for h in expanded_source_macros if OBJECT_LIKE in h])}')
         print(
-            f'    flms: {sum(len(vs) for h, vs in macro_hashes_to_expansion_spelling_locations.items() if FUNCTION_LIKE in h)}')
+            f'    flms: {len([h for h in expanded_source_macros if FUNCTION_LIKE in h])}')
+
+        print(
+            f'unique expansions of source macros: {sum(len(vs) for vs in mhash_to_expansion_spellings_locs.values())}')
+        print(
+            f'    olms: {sum(len(vs) for h, vs in mhash_to_expansion_spellings_locs.items() if OBJECT_LIKE in h)}')
+        print(
+            f'    flms: {sum(len(vs) for h, vs in mhash_to_expansion_spellings_locs.items() if FUNCTION_LIKE in h)}')
 
         # Transform the program to a fixpoint
         num_runs_to_fixpoint = 0
         start_time = datetime.now()
+        # Record transformed macros
+        transformed_mhashes: Set[str] = set()
+        # While transforming, map each macro to its set of reasons why it wasn't transformed
+        mhash_to_reasons_not_transformed: Dict[str, Set[str]] = defaultdict(
+            set)
         while True:
             emitted_a_transformation = False
             for cc in compile_commands:
@@ -170,18 +181,48 @@ def main():
                 for line in cp.stderr.splitlines():
                     if line.startswith(f'CPP2C:{TRANSFORMED_EXPANSION}'):
                         emitted_a_transformation = True
+                        _, mhash, _, _ = line.split(',')
+                        if mhash in expanded_source_macros:
+                            transformed_mhashes.add(mhash)
+                    elif line.startswith(f'CPP2C:{UNTRANSFORMED_EXPANSION}'):
+                        _, mhash, _, reason = line.split(',')
+                        if mhash in expanded_source_macros:
+                            mhash_to_reasons_not_transformed[mhash].add(reason)
 
             num_runs_to_fixpoint += 1
-            if not emitted_a_transformation:
-                break
             print(
                 f'finished run {num_runs_to_fixpoint} of {evaluation_program.name}', file=sys.stderr)
+            if not emitted_a_transformation:
+                break
         end_time = datetime.now()
         elapsed = end_time - start_time
         print(
             f'time to reach a fixpoint: {elapsed.seconds}.{elapsed.seconds}s')
 
         print(f'runs to reach a fixpoint: {num_runs_to_fixpoint}')
+
+        print(
+            f'transformed source macros (based on Cpp2C messages): {len(transformed_mhashes)}')
+        print(
+            f'    olms: {len([h for h in transformed_mhashes if OBJECT_LIKE in h])}')
+        print(
+            f'    flms: {len([h for h in transformed_mhashes if FUNCTION_LIKE in h])}')
+
+        # Remove from the list of untransformed macros those that were transformed at least once,
+        # because a macro may have not been transformed in an early run and then transformed in
+        # a later run
+        # NOTE: We base this on the annotations seen while transforming, not while checking for
+        # invocations
+        untransformed_mhashes = {mhash for mhash
+                                 in mhash_to_reasons_not_transformed
+                                 if mhash not in transformed_mhashes}
+
+        print(
+            f'untransformed macros (based on Cpp2C messages): {len(untransformed_mhashes)}')
+        print(
+            f'    olms: {len([h for h in untransformed_mhashes if OBJECT_LIKE in h])}')
+        print(
+            f'    flms: {len([h for h in untransformed_mhashes if FUNCTION_LIKE in h])}')
 
         # Deduplicate transformed macros in all files
         # for cc in compile_commands:
@@ -203,7 +244,7 @@ def main():
         # Obstacles:
         # - One macro may have many distinct transformed definitions
         #   spanning multiple files, each with a potentially different
-        #   type signatures and
+        #   type signature
         # - If a macro contains nested invocations, then the transformed
         #   definition may end up containing transformed invocations.
         #   A macro may be transformed multiple times though, so we should
@@ -218,60 +259,56 @@ def main():
 
         # Step 2: Search for unique transformed invocations
 
-        # Define a function for hashing macros from an annotation
-
-        def macro_hash_from_annotation(annotation: Dict) -> str:
-            return (annotation['macro name'] + ";" +
-                    annotation['macro type'] + ";" +
-                    annotation['macro definition realpath'] + ";" +
-                    str(annotation['macro definition number']))
-
         # Map each macro to a single transformed declaration under which to search
         # transformed invocations under
-        seen_macro_hashes: Set[str] = set()
+        seen_mhashes: Set[str] = set()
         canonical_transformed_decl_names: Set[str] = set()
         for name, annotation in transformed_names_to_annotations.items():
             mhash = macro_hash_from_annotation(annotation)
-            if mhash not in seen_macro_hashes:
+            if mhash not in seen_mhashes:
+                seen_mhashes.add(mhash)
                 canonical_transformed_decl_names.add(name)
-                seen_macro_hashes.add(mhash)
 
         # Map each transformed decl name to its macro hash
-        transformed_decl_names_to_macro_hashes = {
+        transformed_name_to_mhash = {
             name: macro_hash_from_annotation(annotation)
             for name, annotation in transformed_names_to_annotations.items()
         }
 
-        print(
-            f'transformed macros (based on annotations): {len(seen_macro_hashes)}')
-        print(
-            f'    olms: {len([h for h in seen_macro_hashes if OBJECT_LIKE in h])}')
-        print(
-            f'    flms: {len([h for h in seen_macro_hashes if FUNCTION_LIKE in h])}')
-
         macro_hash_to_unique_transformed_invocations: Dict[str, int] = defaultdict(
+            int)
+        macro_hash_to_unique_transformed_invocations_olms: Dict[str, int] = defaultdict(
+            int)
+        macro_hash_to_unique_transformed_invocations_flms: Dict[str, int] = defaultdict(
             int)
         for cc in compile_commands:
             hash_count_for_file = clang_tools.map_macro_hashes_to_unique_transformed_invocations(
                 cc,
-                transformed_decl_names_to_macro_hashes,
+                transformed_name_to_mhash,
                 canonical_transformed_decl_names)
             for mhash, count in hash_count_for_file.items():
                 macro_hash_to_unique_transformed_invocations[mhash] += count
+                if OBJECT_LIKE in mhash:
+                    macro_hash_to_unique_transformed_invocations_olms[mhash] += count
+                elif FUNCTION_LIKE in mhash:
+                    macro_hash_to_unique_transformed_invocations_flms[mhash] += count
 
-        print(
-            f'transformed macros (based on found transformed invocations): {len(macro_hash_to_unique_transformed_invocations)}')
-        print(
-            f'    olms: {len([h for h in macro_hash_to_unique_transformed_invocations if OBJECT_LIKE in h])}')
-        print(
-            f'    flms: {len([h for h in macro_hash_to_unique_transformed_invocations if FUNCTION_LIKE in h])}')
+        # print(
+        #     f'transformed macros (based on found transformed invocations): {len(macro_hash_to_unique_transformed_invocations)}')
+        # print(
+        #     f'    olms: {len(macro_hash_to_unique_transformed_invocations_olms)}')
+        # print(
+        #     f'    flms: {len(macro_hash_to_unique_transformed_invocations_flms)}')
+
+        # print(
+        #     f'difference: {set(transformed_mhashes).difference(set(macro_hash_to_unique_transformed_invocations.keys()))}')
 
         unique_transformed_invocations = list(
             macro_hash_to_unique_transformed_invocations.values())
-        unique_transformed_invocations_olms = [
-            count for h, count in macro_hash_to_unique_transformed_invocations.items() if OBJECT_LIKE in h]
-        unique_transformed_invocations_flms = [
-            count for h, count in macro_hash_to_unique_transformed_invocations.items() if FUNCTION_LIKE in h]
+        unique_transformed_invocations_olms = list(
+            macro_hash_to_unique_transformed_invocations_olms.values())
+        unique_transformed_invocations_flms = list(
+            macro_hash_to_unique_transformed_invocations_flms.values())
 
         print(
             f'unique transformed invocations: {sum(unique_transformed_invocations)}')
@@ -290,27 +327,28 @@ def main():
         print(f'    olms: {twenty_num(unique_transformed_invocations_olms)}')
         print(f'    flms: {twenty_num(unique_transformed_invocations_flms)}')
 
+        # FIX!
+
         top_five_most_transformed_macros = [
-            (mhash.split(';')[0], count)
-            for mhash, count in sorted(
+            (h.split(';')[0], count)
+            for h, count in sorted(
                 macro_hash_to_unique_transformed_invocations.items(),
                 reverse=True,
                 key=lambda pair: pair[1])[0:5]]
 
         top_five_most_transformed_olms = [
-            (mhash.split(';')[0], count)
-            for mhash, count in sorted(
-                macro_hash_to_unique_transformed_invocations.items(),
+            (h.split(';')[0], count)
+            for h, count in sorted(
+                macro_hash_to_unique_transformed_invocations_olms.items(),
                 reverse=True,
-                key=lambda pair: pair[1])[0:5]
-            if OBJECT_LIKE in mhash]
+                key=lambda pair: pair[1])[0:5]]
+
         top_five_most_transformed_flms = [
-            (mhash.split(';')[0], count)
-            for mhash, count in sorted(
-                macro_hash_to_unique_transformed_invocations.items(),
+            (h.split(';')[0], count)
+            for h, count in sorted(
+                macro_hash_to_unique_transformed_invocations_flms.items(),
                 reverse=True,
-                key=lambda pair: pair[1])[0:5]
-            if FUNCTION_LIKE in mhash]
+                key=lambda pair: pair[1])[0:5]]
 
         print(
             f'top five most transformed macros: {top_five_most_transformed_macros}')
