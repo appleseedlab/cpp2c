63b8a49c81be9d93385a2172c21e677841c7c6ef
Brent Pappas <pappasbrent@gmail.com>
2022-08-13 16:25:34 -0400

    add several null checks
    check that anonymous definition is not null before transforming

--- implementation/src/Visitors/DeanonymizerVisitor.cc
+++ implementation/src/Visitors/DeanonymizerVisitor.cc
@@ -5,27 +5,27 @@
 
 namespace Visitors
 {
-    using clang::TypedefDecl;
 
     DeanonymizerVisitor::DeanonymizerVisitor(
         clang::ASTContext &Ctx,
         clang::Rewriter &RW) : Ctx(Ctx), RW(RW) {}
 
     bool DeanonymizerVisitor::VisitTypedefDecl(
-        TypedefDecl *TD)
+        clang::TypedefDecl *TD)
     {
         // Is this typedef hiding a struct/union/enum decl?
         clang::QualType QT = TD->getUnderlyingType();
         const clang::Type *T = QT.getTypePtr();
-        if (T->isStructureType() || T->isUnionType() || T->isEnumeralType())
+        if (T &&
+            (T->isStructureType() || T->isUnionType() || T->isEnumeralType()))
         {
-            // llvm::errs() << "Found an typedef of a struct/union/enum " << TD->getName().str() << "\n";
+            llvm::errs() << "Found an typedef of a struct/union/enum " << TD->getName().str() << "\n";
             // Is this type an anonymous type?
             const clang::TagDecl *TaD = T->getAsTagDecl();
-            if (TaD->getDeclName().isEmpty())
+            if (TaD && TaD->getDeclName().isEmpty())
             {
                 auto TDName = TD->getName();
-                // llvm::errs() << "typedef'd struct/union/enum " << TDName.str() << " is anonymous\n";
+                llvm::errs() << "typedef'd struct/union/enum " << TDName.str() << " is anonymous\n";
 
                 // If so, then rewrite it to not be anonymous by
                 // inserting the typedef name just before the definition
@@ -33,29 +33,27 @@ namespace Visitors
                 auto &SM = Ctx.getSourceManager();
                 auto Def = TaD->getDefinition();
 
-                // {
-                //     if (Def == nullptr)
-                //     {
-                //         llvm::errs() << "Deanonymizing a typedef'd struct/union/enum without a definition: " << TDName.str() << "\n";
-                //     }
-                //     else
-                //     {
-                //         llvm::errs() << "Deanonymizing a typedef'd struct/union/enum with a definition: " << TDName.str() << "\n";
-                //     }
-                // }
-
-                auto LBraceLoc = Def->getBraceRange().getBegin();
-                LBraceLoc = SM.getExpansionLoc(LBraceLoc);
-
-                // Don't rewrite in standard headers
-                if (!Utils::isInStdHeader(LBraceLoc, SM))
+                if (Def == nullptr)
                 {
-                    auto RWText = TDName.str() + " ";
-                    bool failed = RW.InsertTextBefore(LBraceLoc, RWText);
-                    if (failed)
+                    llvm::errs() << "Deanonymizing a typedef'd struct/union/enum without a definition: " << TDName.str() << "\n";
+                }
+                else
+                {
+                    llvm::errs() << "Deanonymizing a typedef'd struct/union/enum with a definition: " << TDName.str() << "\n";
+
+                    auto LBraceLoc = Def->getBraceRange().getBegin();
+                    LBraceLoc = SM.getExpansionLoc(LBraceLoc);
+
+                    // Don't rewrite in standard headers
+                    if (!Utils::isInStdHeader(LBraceLoc, SM))
                     {
-                        llvm::errs() << "Failed to deanonymize struct/union/enum " << TDName.str() << "\n";
-                        assert(false);
+                        auto RWText = TDName.str() + " ";
+                        bool failed = RW.InsertTextBefore(LBraceLoc, RWText);
+                        if (failed)
+                        {
+                            llvm::errs() << "Failed to deanonymize struct/union/enum " << TDName.str() << "\n";
+                            assert(false);
+                        }
                     }
                 }
             }
