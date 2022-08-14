2acc8e29683698cca8df97e3ed1d38fc61d72b44
Brent Pappas <pappasbrent@gmail.com>
2022-08-14 10:20:15 -0400

    change tag decls tests to test that macros using anonymous structs are not transformed

--- implementation/tests/structs_unions_enums.c
+++ implementation/tests/structs_unions_enums.c
@@ -14,11 +14,18 @@ union U
     S s;
 };
 
-// This should be made not anonymous
+// Any transformed definitions containing
+// anonymous types should not be transformed
 typedef enum
 {
     Y = 0
 } M;
+
+struct T {
+    union {
+        S s;
+    } u;
+};
 
 M f(M m) { return m; }
 #define CALL_F_WITH(N) f(N)
@@ -26,19 +33,30 @@ M f(M m) { return m; }
 int main(int argc, char const *argv[])
 {
     S s;
+    // should be transformed
     GET_X_STATIC(s);
+    // should be transformed
     GET_X_STATIC(s);
+    // should be transformed
     GET_X_PTR((&s));
 
     union U u;
+    // should be transformed
     GET_X_STATIC(u);
+    // should be transformed
     GET_S_STATIC(u);
 
     M m;
+    // should not be transformed
     CALL_F_WITH(m);
+
+    struct T t;
+    // should not be transformed
+    GET_S_STATIC(t.u);
 
 #undef GET_X_PTR
 #define GET_X_PTR(su) su->x
+    // should be transformed
     GET_X_PTR((&s));
     return 0;
 }
