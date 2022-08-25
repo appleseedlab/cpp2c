14ddb109c86fc0c44fd8c54c2c286e4fa1967930
Brent Pappas <pappasbrent@gmail.com>
2022-08-25 11:06:38 -0400

    check that invocations that should be transformed are, and that ones that should not have be transformed are not

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,24 @@
 # Assumptions
 # - cpp2c has already been made
 
+# Reads the file line-by-line, and if a line contains
+# the search string, echoes out the following line
+function echo_lines_after_text {
+    P_LINE=false
+    NEEDLE="$1"
+    while read line
+    do
+        if $P_LINE; then
+            echo "$line"
+            P_LINE=false
+        fi
+
+        if echo "$line" | grep -q "$NEEDLE"; then
+            P_LINE=true
+        fi
+    done < "$2"
+}
+
 # delete copied files
 rm -f implementation/tests/*.cpy.*
 
@@ -33,7 +51,7 @@ do
 
     # compile the src file
     cc $F
-    
+
     # run it and save its output
     ./a.out >original.txt
 
@@ -63,6 +81,43 @@ do
     RES=$(cmp original.txt transformed.txt)
 
     if [ $? -ne 0 ]
+    then
+        echo "test $i: $(basename $F) fail"
+        let fail++
+        let i++
+        continue
+    fi
+
+    # Check that every invocation that should have been transformed was
+    # transformed
+    ORIGINAL=$(echo_lines_after_text "Should transform" "$F")
+    TRANSFORMED=$(echo_lines_after_text "Should transform" "$F_CPY")
+    IFS=$'\n' read -d '' -a o_lines < <(echo "$ORIGINAL")
+    IFS=$'\n' read -d '' -a t_lines < <(echo "$TRANSFORMED")
+    LEN=${#o_lines[@]}
+
+    FAILED=false
+    for (( j = 0 ; j < $LEN ; j++ ))
+    do
+        if [ "${o_lines[$j]}" == "${t_lines[$j]}" ]
+        then
+            echo "Not transformed: ${o_lines[$j]}"
+            FAILED=true
+        fi
+    done
+    if $FAILED
+    then
+        echo "test $i: $(basename $F) fail"
+        let fail++
+        let i++
+        continue
+    fi
+
+    # Check that every invocation that should not have been transformed
+    # was not transformed
+    ORIGINAL=$(echo_lines_after_text "Should not transform" "$F")
+    TRANSFORMED=$(echo_lines_after_text "Should not transform" "$F_CPY")
+    if ! diff <(echo "$ORIGINAL") <(echo "$TRANSFORMED")
     then
         echo "test $i: $(basename $F) fail"
         let fail++
